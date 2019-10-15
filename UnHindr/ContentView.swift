2459d192d2849fa2637e3875679f672031d99550
ata87 <ata87@sfu.ca>
2019-10-14 18:27:28 -0700

    UnHindr application setup with Firebase

---
+++ UnHindr/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  UnHindr
+//
+//  Created by Allan on 2019-10-07.
+//  Copyright © 2019 Allan. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
