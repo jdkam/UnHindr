2c27bb065080b338ac408344f473ef85aa7aa783
ata87 <ata87@sfu.ca>
2019-10-14 22:43:46 -0700

    Rebuit Xcode project with unit testing and storyboard

---
+++ UnHindr/UnHindr/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UnHindr
+//
+//  Created by Allan on 2019-10-14.
+//  Copyright © 2019 Sigma. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
