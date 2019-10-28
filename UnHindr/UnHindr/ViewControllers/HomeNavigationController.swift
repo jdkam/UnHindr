9e2d74475191b9a993ddd422b451cba2561f29f4
Allan Tsai <31875809+allantsai3@users.noreply.github.com>
2019-10-28 00:08:21 -0700

    Revert "merged template into master"
    This reverts commit 5fecb757c346dee6f8113cbbeab8df3c594745ec, reversing
    changes made to a06027b61bf99b4ab10b8772cee0fd0e5d55436e.

---
+++ UnHindr/UnHindr/ViewControllers/HomeNavigationController.swift
@@ -0,0 +1,16 @@
+//
+//  HomeNavigationController.swift
+//  UnHindr
+//
+//  Created by Allan on 2019-10-25.
+//  Copyright © 2019 Sigma. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class HomeNavigationController: UINavigationController {
+    override func viewDidLoad(){
+        super.viewDidLoad()
+    }
+}
