5e0a4a13ad05a1694d69e783ee057b899fb78b0c
Sina Ahmadian Behrouz <ahmadian@asb9700-e03.csil.sfu.ca>
2019-11-04 00:20:22 -0800

    Merge conflict fix

--- UnHindr/ViewControllers/Medication/MedicationHomeViewController.swift
+++ UnHindr/ViewControllers/Medication/MedicationHomeViewController.swift
@@ -1,36 +1,41 @@
-//
-//  MedicationHomeViewController.swift
-//  UnHindr
-//
-//  Created by Allan on 2019-11-03.
-//  Copyright © 2019 Sigma. All rights reserved.
-//
+/*
+ File: [MedicationHomeViewController.swift]
+ Creators: [Allan, Jordan]
+ Date created: [03/11/2019]
+ Date updated: [03/11/2019]
+ Updater name: [Jordan]
+ File description: [Controls the MyMeds screen in UnHindr]
+ */
 
 import UIKit
 
 class MedicationHomeViewController: UIViewController {
-    @IBOutlet weak var MedicineLabel: UILabel!
-    @IBOutlet weak var DosageLabel: UILabel!
-    @IBOutlet weak var QuantityLabel: UILabel!
-    
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
     }
     
-    
-    
-    @IBAction func CheckTapped(_ sender: UIButton) {
-    }
-    
-    @IBAction func CancelTapped(_ sender: UIButton) {
-    }
-    
-    
+    // MARK: - Transitions storyboard to Home Menu
+    // Input:
+    //      Home button tapped
+    // Output:
+    //      Switch from Medication to Home menu
     @IBAction func goToHomeTapped(_ sender: Any) {
         let storyboard = UIStoryboard(name: "HomeScreen", bundle: nil)
         let vc = storyboard.instantiateViewController(withIdentifier: "HomeScreenViewController") as UIViewController
         present(vc, animated: true, completion: nil)
     }
-
+    
+    /*
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
