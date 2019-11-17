ec6ee8f3988b1aec4451bf9142531f17e1efe733
Wei <wla143@sfu.ca>
2019-11-16 18:53:00 -0800

    SignUp page gender picker coded

--- UnHindr/SignUp/SignUpViewController.swift
+++ UnHindr/SignUp/SignUpViewController.swift
@@ -8,14 +8,21 @@
 import Foundation
 import UIKit
 
-class SignUpViewController: UIViewController {
+class SignUpViewController: UIViewController, UIPickerViewDataSource,UIPickerViewDelegate {
 
     @IBOutlet weak var DatePicker: UITextField!
+    @IBOutlet weak var Gender: UITextField!
     
     private var datePicker: UIDatePicker?
+    var gender = ["Male", "Female", "Undefined"]
+    var picker = UIPickerView()
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        picker.delegate = self
+        picker.dataSource = self
+        Gender.inputView = picker
         
         datePicker = UIDatePicker()
         datePicker?.datePickerMode = .date
@@ -40,7 +47,22 @@ class SignUpViewController: UIViewController {
     @objc func viewTapped(gestureRecognizer: UITapGestureRecognizer){
         view.endEditing(true)
     }
-
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return gender.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        Gender.text = gender[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return gender[row]
+    }
     /*
     // MARK: - Navigation
 
