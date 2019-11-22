8dac19cd6d8bfcc310ca4da283e41e45ae1b919b
Jordan Kam <jdkam@sfu.ca>
2019-11-22 15:27:14 -0800

    Initial Commit. Uploaded image assets needed and created initial storyboard for chat view

---
+++ UnHindr/Chat/ChatViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ChatViewController.swift
+//  UnHindr
+//
+//  Created by Jordan Kam on 2019-11-22.
+//  Copyright © 2019 Sigma. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import Firebase
+
+class ChatViewController: UIViewController {
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    
+}
---
+++ UnHindr/Chat/Message.swift
@@ -0,0 +1,14 @@
+//
+//  Message.swift
+//  UnHindr
+//
+//  Created by Jordan Kam on 2019-11-22.
+//  Copyright © 2019 Sigma. All rights reserved.
+//
+
+import Foundation
+
+struct Message {
+    let sender: String
+    let body: String
+}
