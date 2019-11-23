a66347aa95292fbf4ac57732619266ba9cdfc1c8
Jordan Kam <jdkam@sfu.ca>
2019-11-22 17:16:46 -0800

    Finished UI and constraints, and populated tableView with messages

--- UnHindr/Chat/ChatViewController.swift
+++ UnHindr/Chat/ChatViewController.swift
@@ -12,11 +12,43 @@ import Firebase
 
 class ChatViewController: UIViewController {
     
+    
     @IBOutlet weak var tableView: UITableView!
+    
+    var messages: [Message] = [
+        Message(sender: "unittestacc@gmail.com", body: "Hi!"),
+        Message(sender: "unittestacc1@gmail.com", body: "hey"),
+        Message(sender: "unittestacc@gmail.com", body: "How are you?")
+
+    ]
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.dataSource = self
+    }
+    
+
+    @IBOutlet weak var messageTextField: UITextField!
+    
+    @IBAction func sendPressed(_ sender: UIButton) {
     }
     
     
 }
+
+//Protocol for managing the populating of the tableView
+extension ChatViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return messages.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ReusableCell", for: indexPath)
+        
+        cell.textLabel?.text = messages[indexPath.row].body
+        return cell
+    }
+    
+}
+
+
