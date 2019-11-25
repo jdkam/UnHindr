4ea18d1186cdc247c6fd98c4480fd7eb9d7dfa06
Jordan Kam <jdkam@sfu.ca>
2019-11-24 17:17:05 -0800

    Updated AppDelegate file to include KeyboardManager

--- UnHindr/AppDelegate.swift
+++ UnHindr/AppDelegate.swift
@@ -9,6 +9,7 @@
 
 import UIKit
 import Firebase
+import IQKeyboardManagerSwift
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -19,7 +20,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         FirebaseApp.configure()
-        
+        IQKeyboardManager.shared.enable = true
+        IQKeyboardManager.shared.enableAutoToolbar = false
+        IQKeyboardManager.shared.shouldResignOnTouchOutside = true
         return true
     }
 
